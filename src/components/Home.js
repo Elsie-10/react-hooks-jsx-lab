3a0ff76d249c15000a68ca8daf1a84d373796d85
Elsie <elsieoscar06@gmail.com>
2025-04-08 18:44:53 +0300

    react

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return React.createElement(
+    "div",
+    { id: "home" },
+    React.createElement(
+      "h1",
+      { style: { color: "firebrick" } },
+      `${name} is a Web Developer from ${city}`
+    )
+  );
 }
 
 export default Home;
